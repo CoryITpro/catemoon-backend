fdcb801b73f05af2b8a9682f5a02fece4f869dfb
Cory <coryitpro@gmail.com>
2021-12-02 14:14:18 -0500

    Update the configuration

--- src/configs/index.js
+++ src/configs/index.js
@@ -13,8 +13,15 @@ module.exports = {
   host: process.env.HOST || "127.0.0.1",
   port: process.env.PORT || "3000",
   // Development Environments
-  mongoURL: process.env.MONGL_URL || "",
+  mongoURL:
+    process.env.MONGL_URL ||
+    "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false",
   jwtSecret: process.env.JWT_SECRET || "$ecret",
   jwtExpires: process.env.JWT_EXPIRES || "30d",
   apiRoot: process.env.API_ROOT || "/",
+  // Twitter Environments
+  twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY || "",
+  twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET || "",
+  twitterAccessToken: process.env.TWITTER_ACCESS_TOKEN || "",
+  twitterAccessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET || "",
 };
