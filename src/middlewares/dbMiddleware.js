6aa659cb98cda46de17b227b56702f7423938b75
Cory <coryitpro@gmail.com>
2021-12-02 14:12:47 -0500

    Connect mongodb

--- src/middlewares/dbMiddleware.js
+++ src/middlewares/dbMiddleware.js
@@ -4,28 +4,19 @@ const logger = require("../utils/logger");
 
 module.exports = (callback) => {
   mongoose.Promise = global.Promise;
-  mongoose.connect(
-    configs.mongoURL,
-    {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    },
-    (err) => {
-      if (err) {
-        logger.error(
-          err.message ||
-            "# Please install MongoDB and make sure it is running #"
-        );
+  mongoose.connect(configs.mongoURL, (err) => {
+    if (err) {
+      logger.error(
+        err.message || "# Please install MongoDB and make sure it is running #"
+      );
 
-        throw err;
-      }
-
-      logger.log("DB: Mongo DB has been connected :DB");
-
-      if (typeof callback === "function") {
-        callback();
-      }
+      throw err;
     }
-  );
+
+    logger.log("DB: Mongo DB has been connected :DB");
+
+    if (typeof callback === "function") {
+      callback();
+    }
+  });
 };
