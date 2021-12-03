d534f499a39f2da22b37fbbf5790c4c3a3591359
Cory <coryitpro@gmail.com>
2021-12-03 08:36:49 -0500

    Update

--- src/apis/controllers/reward.js
+++ src/apis/controllers/reward.js
@@ -5,23 +5,6 @@ const logger = require("../../utils/logger");
 
 const RESPONSE_STATE = require("../../constants/responseState");
 
-const getUserInfo = (screen_names, id_list) =>
-  Twitter.get("users/lookup", { user_id: id_list })
-    .then(({ data }) => {
-      screen_names.push(data);
-    })
-    .catch((err) => {
-      if (err) {
-        logger.error(err.message);
-
-        res.status(RESPONSE_STATE.INTERNAL_ERROR).json({
-          message:
-            "There was an error getting user account screen name from twitter",
-        });
-        next();
-      }
-    });
-
 const linkTwitter = (req, res, next) => {
   // Try to find if there is valid user with req.body.walletId
   User.findOne({ walletId: req.body.walletId })
@@ -36,101 +19,18 @@ const linkTwitter = (req, res, next) => {
           twitter: req.body.twitter,
         });
 
-        Twitter.get("account/verify_credentials", { skip_status: true })
+        Twitter.get(`/2/users/:${req.body.walletId}`)
           .then(({ data }) => {
-            logger.log(`Data: ${data}`);
             res.json({
               message: data,
             });
           })
           .catch((err) => {
             res.status(RESPONSE_STATE.INTERNAL_ERROR).json({
-              message: err.stack,
+              message: `There was an error while getting twitter account status ${err.stack}`,
             });
             next();
           });
-
-        // // Get follower list of given twitter handle
-        // Twitter.get("followers/ids", { screen_name: req.body.twitter })
-        //   .then(({ data }) => {
-        //     // Get id list
-        //     const followers = data.ids;
-
-        //     logger.log(
-        //       `Holder counts of @${req.body.twitter}: ${followers.length}`
-        //     );
-
-        //     // Divide substring of 100 ids
-        //     const requestNumber = Math.floor(followers.length / 100);
-        //     const remains = followers.length % 100;
-
-        //     let screen_names = [];
-        //     let get_name_requests = [];
-
-        //     // Make the list of promises
-        //     for (let i = 0; i < requestNumber; i++) {
-        //       get_name_requests.push(
-        //         getUserInfo(
-        //           screen_names,
-        //           followers.slice(i * 100, i * 100 + 100).join(",")
-        //         )
-        //       );
-        //     }
-
-        //     if (remains !== 0) {
-        //       get_name_requests.push(
-        //         getUserInfo(
-        //           screen_names,
-        //           followers
-        //             .slice(requestNumber * 100, requestNumber * 100 + 100)
-        //             .join(",")
-        //         )
-        //       );
-        //     }
-
-        //     // Handle every promises
-        //     Promise.all(get_name_requests)
-        //       .then(() => {
-        //         screen_names.map((data) => {
-        //           data.map((data) => {
-        //             logger.log(`follower: ${data.name}, ${data.screen_name}`);
-
-        //             if (data.screen_name === "verifed") {
-        //               user.verified = true;
-
-        //               user.save().then((newUser) => {
-        //                 res.status(RESPONSE_STATE.OKAY).json({
-        //                   message: `Your address has been successfuly connected with verified account ${newUser.twitter}`,
-        //                 });
-        //                 next();
-        //               });
-        //             }
-        //           });
-        //         });
-        //       })
-        //       .catch((err) => {
-        //         res.status(RESPONSE_STATE.INTERNAL_ERROR).json({
-        //           message: err.message,
-        //         });
-        //         next();
-        //       });
-
-        //     user.save().then((newUser) => {
-        //       res.status(RESPONSE_STATE.OKAY).json({
-        //         message: `Your address has been successfuly connected with unverified account ${newUser.twitter}`,
-        //       });
-        //       next();
-        //     });
-        //   })
-        //   .catch((err) => {
-        //     logger.error(err);
-
-        //     res.status(RESPONSE_STATE.INTERNAL_ERROR).json({
-        //       message:
-        //         "There was an error getting user account status from twitter",
-        //     });
-        //     next();
-        //   });
       }
       // If there is such user with given address and twitter screen name
       else {
